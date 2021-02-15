e48120fb14ac306758e5cf1f05445dddc598f284
javo-desu <32529489+javo-desu@users.noreply.github.com>
2021-02-15 15:44:43 -0300

    correccion names componentes lo mas visto y lo mas vendido

--- resources/views/Components/loMvisto.blade.php
+++ resources/views/Components/loMvisto.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-<div class="max-w-6xl bk-hero-main mx-auto sm:px-6 lg:px-8">
+<div class="max-w-full bk-hero-main mx-auto sm:px-6 lg:px-8">
   <section class="hero bk-hero-int is-primary is-medium mt-8
     overflow-hidden shadow sm:rounded-lg">
     <!-- Hero head: will stick at the top -->
@@ -10,10 +10,9 @@
     {{-- producto card --}}
     <div class="mt-8 bg-white bk-hero-bread overflow-hidden shadow sm:rounded-lg">
       <div class="grid grid-cols-3 gap-4">
+      
         <div>1</div>
-        <div>1</div>
-        <!-- ... -->
-        <div>9</div>
+ 
       </div>
     </div>
 </div>
